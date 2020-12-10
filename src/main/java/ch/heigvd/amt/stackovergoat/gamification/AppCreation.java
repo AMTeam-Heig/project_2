d5e8e928cceca0da22cc75bb1982e64103fdda02
ClarFleu <clarisse.fleu@gmail.com>
2020-12-10 22:28:03 +0100

    Application creation works

---
+++ src/main/java/ch/heigvd/amt/stackovergoat/gamification/AppCreation.java
@@ -0,0 +1,12 @@
+package ch.heigvd.amt.stackovergoat.gamification;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+@AllArgsConstructor
+public class AppCreation {
+    private String name;
+}
