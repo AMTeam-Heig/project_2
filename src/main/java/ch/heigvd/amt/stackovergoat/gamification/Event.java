799910edbdf30983398a19efc2777ed53044b2e1
ClarFleu <clarisse.fleu@gmail.com>
2020-12-10 22:42:24 +0100

    Add all needed classes

--- src/main/java/ch/heigvd/amt/stackovergoat/gamification/Event.java
+++ src/main/java/ch/heigvd/amt/stackovergoat/gamification/Event.java
@@ -10,7 +10,7 @@ import lombok.Setter;
 @EqualsAndHashCode
 @Builder(toBuilder = true)
 public class Event {
+    private String username;
     private String name;
     private int points;
-    private String username;
 }
